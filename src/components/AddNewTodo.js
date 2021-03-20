43fe07134e5dcbe028719d0087968271f0ad64cc
yuksel <yukselbey35@gmail.com>
2021-03-20 15:20:26 -0700

    Created Modal.js and style it

--- src/components/AddNewTodo.js
+++ src/components/AddNewTodo.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Modal from './Modal'
 
 function AddNewTodo(){
+    const [showModal, setShowModal] = useState(false)
+
     return (
         <div className='AddNewTodo'>
-            AddNewTodo
+            <div className="btn">
+                <button onClick={() => setShowModal(true)}>
+                    + New Todo
+                </button>
+            </div>
+            <Modal showModal={showModal} setShowModal={setShowModal}>
+                <div>
+                    Hello World
+                    <button
+                    onClick={() => setShowModal(false)}>
+                        hide
+                    </button>
+                </div>
+            </Modal>
         </div>
     )
 }
